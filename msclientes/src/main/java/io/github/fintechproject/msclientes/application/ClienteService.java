39f1e1912a5943023fe167ce47da2a7a91e37a0b
Guilherme Pimentel <guilhermeoscp@gmail.com>
2025-06-26 09:58:49 -0300

    Implementacao do microservice de clientes

---
+++ msclientes/src/main/java/io/github/fintechproject/msclientes/application/ClienteService.java
@@ -0,0 +1,27 @@
+package io.github.fintechproject.msclientes.application;
+
+import io.github.fintechproject.msclientes.domain.Cliente;
+import io.github.fintechproject.msclientes.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    public ClienteService(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Cliente save (Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf) {
+        return repository.findByCpf(cpf);
+    }
+}
